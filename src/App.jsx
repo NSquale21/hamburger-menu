85989b0c8d36e59efb72f8425fb58c976b3aeb7e
Nicole Pasquale <NSquale212@gmail.com>
2020-11-05 11:17:42 -0600

    Add ref & outside click hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
+import { useOnClickOutside } from './hooks';
 import { Burger, Menu } from './components';
 
 function App() {
     
     const [open, setOpen] = React.useState(false);
+
+    const node = React.useRef();
+    useOnClickOutside(node, () => setOpen(false));
     
     return (
         <ThemeProvider theme={theme}>
@@ -17,7 +21,7 @@ function App() {
                     <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
                     <small>Icon made by Freepik from www.flaticon.com</small>
                 </div>
-                <div>
+                <div ref={node}>
                     <Burger open={open} setOpen={setOpen} />
                     <Menu open={open} setOpen={setOpen} />
                 </div>
